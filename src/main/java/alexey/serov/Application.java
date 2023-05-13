131285d7391c5d15000f7bafecdc51995060b677
LeshaSerov <93645965+LeshaSerov@users.noreply.github.com>
2023-05-14 00:19:11 +0400

    slow work

--- src/main/java/alexey/serov/Application.java
+++ src/main/java/alexey/serov/Application.java
@@ -6,23 +6,24 @@ import java.util.List;
 import java.util.Set;
 
 public class Application {
-    List<Set<Integer>> setList = new ArrayList<>(
-            List.of(
-                    Set.of(1),
-                    Set.of(2),
-                    Set.of(3),
-                    Set.of(4),
-                    Set.of(5),
-                    Set.of(6),
-                    Set.of(7),
-                    Set.of(8)
-            )
-    );
-    Set<Integer> set = new HashSet<>(Set.of(1,2,3,4,5,6,7,8,9,10));
+    static List<Set<Integer>> setList = new ArrayList<>(List.of(
+            Set.of(1),
+            Set.of(2),
+            Set.of(1, 2),
+            Set.of(3, 4),
+            Set.of(5, 6),
+            Set.of(1, 3),
+            Set.of(2, 4),
+            Set.of(2, 4, 6),
+            Set.of(1, 3, 5),
+            Set.of(2, 3, 4)
+//            Set.of(3, 4, 5, 6)
+//            Set.of(1, 2, 5, 6)
+    ));
+    static Set<Integer> set = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
 
     public static void main(String[] args) {
-
-
+        new Packer(set, setList).maxPack();
     }
 
 }
--- src/main/java/alexey/serov/Packer.java
+++ src/main/java/alexey/serov/Packer.java
@@ -1,79 +1,79 @@
 package alexey.serov;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import lombok.var;
 
 import java.util.*;
 
 @Slf4j
 public class Packer {
-    private Set<Pack> packs;
     private final Set<Integer> mainSet;
     private final List<Set<Integer>> subsetList;
+    private Set<Set<Set<Integer>>> packSet;
 
     public Packer(Set<Integer> mainSet, List<Set<Integer>> subsetList) {
         this.mainSet = Collections.synchronizedSet(mainSet);
         this.subsetList = Collections.synchronizedList(subsetList);
-        this.packs = Collections.synchronizedSet(new HashSet<Pack>());
+        this.packSet = Collections.synchronizedSet(new HashSet<>());
     }
 
     @Getter
+    @ToString
     private static class Pack {
         Set<Integer> set;
-        ArrayList<HashSet<Integer>> list;
+        List<Set<Integer>> list;
 
-        public Pack(){
-            set = new HashSet<Integer>();
-            list = new ArrayList<HashSet<Integer>>();
+        public Pack() {
+            this.set = new HashSet<>();
+            this.list = new ArrayList<>();
         }
+
         public Pack(Pack pack) {
-            set = pack.getSet();
-            list = pack.getList();
+            this.set = new HashSet<>(pack.getSet());
+            this.list = new ArrayList<>(pack.getList());
         }
 
         public boolean add(Set<Integer> set) {
             if (Collections.disjoint(this.set, set)) {
                 this.set.addAll(set);
-                this.list.add((HashSet<Integer>) set);
+                this.list.add(set);
                 return true;
             }
             return false;
         }
+
     }
 
-    ArrayList<HashSet<Integer>> maxPack() {
-
-
+    public List<Set<Integer>> maxPack() {
         for (var element : subsetList) {
-            List<Set<Integer>> availableSubsets = new ArrayList<>();
-            Collections.copy(subsetList, availableSubsets);
-            process(element, new Pack(), availableSubsets);
+            process(element, new Pack(), new ArrayList<>(subsetList));
         }
 
-
         int length = 0;
-        ArrayList<HashSet<Integer>> result = null;
-        for (var element : packs) {
-            length = Math.max(element.getList().size(), length);
-            result = element.getList();
+        List<Set<Integer>> result = null;
+        for (var element : packSet) {
+            log.atInfo().log(element.toString());
+            length = Integer.max(element.size(), length);
         }
         return result;
     }
 
-    void process(Set<Integer> subset, Pack pack, List<Set<Integer>> availableSubsets) {
-        log.atInfo().log(subset.toString() + pack.toString() + availableSubsets.toString());
-        if (pack.add((HashSet<Integer>) subset) && pack.getSet() == mainSet) {
-            packs.add(pack);
+    private void process(Set<Integer> subset, Pack pack, List<Set<Integer>> availableSubsets) {
+//        log.atInfo().log(subset.toString() + " " + pack.toString() + " " + availableSubsets.toString());
+
+        if (pack.add(subset) && mainSet.equals(pack.getSet())) {
+//            log.atInfo().log(pack.getList().toString());
+            packSet.add(new HashSet<>(pack.getList()));
             return;
         }
         availableSubsets.remove(subset);
         if (availableSubsets.isEmpty()) {
+//            log.atInfo().log(pack.getList().toString());
             return;
         }
         for (var element : availableSubsets) {
-            process(element, new Pack(pack), availableSubsets);
+            process(element, new Pack(pack), new ArrayList<>(availableSubsets));
         }
     }
 }
