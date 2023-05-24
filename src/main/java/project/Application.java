374d9e0aad3cff429727ca6e83136b8a6d04bb88
LeshaSerov <93645965+LeshaSerov@users.noreply.github.com>
2023-05-24 23:20:01 +0400

    Optimisation

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -63,40 +63,109 @@ public class Application {
       Set.of(11)
   ));
 
+  static List<Set<Integer>> setList5 = new ArrayList<>(List.of(
+      Set.of(1, 8),
+      Set.of(2, 3),
+      Set.of(2, 4),
+      Set.of(2, 5),
+      Set.of(2, 6),
+      Set.of(2, 7),
+      Set.of(2, 8),
+      Set.of(3, 4),
+      Set.of(3, 5),
+      Set.of(3, 6),
+      Set.of(3, 7),
+      Set.of(3, 8),
+      Set.of(4, 5),
+      Set.of(4, 6),
+      Set.of(4, 7),
+      Set.of(4, 8),
+      Set.of(5, 6),
+      Set.of(5, 7),
+      Set.of(5, 8),
+      Set.of(6, 7),
+      Set.of(6, 8),
+      Set.of(7, 8)
+  ));
+
+  static List<Set<Integer>> setList6 = new ArrayList<>(List.of(
+      Set.of(1),
+      Set.of(2),
+      Set.of(3),
+      Set.of(4),
+      Set.of(5),
+      Set.of(6),
+      Set.of(7),
+      Set.of(8),
+      Set.of(9),
+      Set.of(10),
+      Set.of(11),
+      Set.of(12),
+      Set.of(13),
+      Set.of(14),
+      Set.of(15),
+      Set.of(16),
+      Set.of(17),
+      Set.of(18),
+      Set.of(19),
+      Set.of(20),
+      Set.of(21),
+      Set.of(22),
+      Set.of(23),
+      Set.of(24),
+      Set.of(25),
+      Set.of(26),
+      Set.of(27)
+  ));
+
   static Set<Integer> set = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
   static Set<Integer> set3 = new HashSet<>(Set.of(1, 2, 3, 4));
-  static Set<Integer> set4 = new HashSet<>(Set.of(1, 2, 3, 4,5,6,7,8,9,10,11));
+  static Set<Integer> set4 = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+  static Set<Integer> set5 = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6, 7, 8));
+  static Set<Integer> set6 = new HashSet<>(
+      Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+          25, 26, 27));
 
   public static void main(String[] args) {
     //Настройка отображения всех найденных упаковок
-    boolean showAllPackagesInConsole = true;
+    boolean showAllPackagesInConsole = false;
     //Количество потоков
     int nThreads = Runtime.getRuntime().availableProcessors();
 
-    log.atInfo().log("Поиск максимальной упаковки\n");
-    getMaxPack(nThreads, set, setList, showAllPackagesInConsole);
-    getMaxPack(6, set, setList, showAllPackagesInConsole);
-
-    log.atInfo().log("Поиск максимальной упаковки\n");
-//    //Этот код очень медленный 32 сек
-//    getMaxPack(1, set, setList2, showAllPackagesInConsole);
-    getMaxPack(6, set, setList2, showAllPackagesInConsole);
-
-    log.atInfo().log("Поиск максимальной упаковки\n");
-    getMaxPack(1, set3, setList, showAllPackagesInConsole);
-    getMaxPack(6, set3, setList3, showAllPackagesInConsole);
-
-//    //Этот код медленный
 //    log.atInfo().log("Поиск максимальной упаковки\n");
-//    //102 сек
-//    getMaxPack(1, set4, setList4, showAllPackagesInConsole);
-//    //26 сек
-//    getMaxPack(6, set4, setList4, showAllPackagesInConsole);
+//    getMaxPack(nThreads, set, setList, showAllPackagesInConsole);
+//    getMaxPack(6, set, setList, showAllPackagesInConsole);
+//
+//    log.atInfo().log("Поиск максимальной упаковки\n");
+////    //Этот код очень медленный 32 сек
+//    getMaxPack(1, set, setList2, showAllPackagesInConsole);
+//    getMaxPack(6, set, setList2, showAllPackagesInConsole);
+//
+//    log.atInfo().log("Поиск максимальной упаковки\n");
+//    getMaxPack(1, set3, setList3, showAllPackagesInConsole);
+//    getMaxPack(6, set3, setList3, showAllPackagesInConsole);
+
+    //Этот код медленный
+    log.atInfo().log("Поиск максимальной упаковки\n");
+    //102 сек
+    getMaxPack(1, set4, setList4, showAllPackagesInConsole);
+    //26 сек
+    getMaxPack(6, set4, setList4, showAllPackagesInConsole);
+
+    log.atInfo().log("Поиск максимальной упаковки\n");
+    getMaxPack(nThreads, set5, setList5, showAllPackagesInConsole);
+    getMaxPack(6, set5, setList5, showAllPackagesInConsole);
+
+    log.atInfo().log("Поиск максимальной упаковки\n");
+    getMaxPack(nThreads, set6, setList6, showAllPackagesInConsole);
+    getMaxPack(6, set6, setList6, showAllPackagesInConsole);
 
   }
 
   private static void getMaxPack(int nThreads, Set<Integer> set, List<Set<Integer>> setList,
       boolean showAllPackagesInConsole) {
+//    log.atInfo().log(setList.toString() + " : " + nThreads);
+
     long start = System.nanoTime();
     List<Set<Integer>> maxPack = new Packer(set, setList)
         .maxPack(nThreads, showAllPackagesInConsole);
--- src/main/java/project/Packer.java
+++ src/main/java/project/Packer.java
@@ -1,87 +1,103 @@
 package project;
 
-import java.util.concurrent.locks.LockSupport;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.*;
-import java.util.concurrent.*;
-
 @Slf4j
 public class Packer {
-    private final Set<Integer> mainSet;
-    private final List<Set<Integer>> subsetList;
-    private Set<Set<Set<Integer>>> packSet;
 
-    public Packer(Set<Integer> mainSet, List<Set<Integer>> subsetList) {
-        this.mainSet = Set.copyOf(mainSet);
-        this.subsetList = List.copyOf(subsetList);
-        this.packSet = Collections.newSetFromMap(new ConcurrentHashMap<>());
+  private final Set<Integer> mainSet;
+  private final List<Set<Integer>> subsetList;
+  private Set<Set<Set<Integer>>> packSet;
+
+  public Packer(Set<Integer> mainSet, List<Set<Integer>> subsetList) {
+    this.mainSet = Set.copyOf(mainSet);
+    this.subsetList = List.copyOf(subsetList);
+    this.packSet = Collections.newSetFromMap(new ConcurrentHashMap<>());
+  }
+
+  @Getter
+  @ToString
+  private static class Pack {
+
+    Set<Integer> set;
+    List<Set<Integer>> list;
+
+    public Pack() {
+      this.set = new HashSet<>();
+      this.list = new ArrayList<>();
     }
 
-    @Getter
-    @ToString
-    private static class Pack {
-        Set<Integer> set;
-        List<Set<Integer>> list;
-
-        public Pack() {
-            this.set = new HashSet<>();
-            this.list = new ArrayList<>();
-        }
-
-        public Pack(Pack pack) {
-            this.set = new HashSet<>(pack.getSet());
-            this.list = new ArrayList<>(pack.getList());
-        }
-
-        public boolean add(Set<Integer> set) {
-            if (Collections.disjoint(this.set, set)) {
-                this.set.addAll(set);
-                this.list.add(set);
-                return true;
-            }
-            return false;
-        }
+    public Pack(Pack pack) {
+      this.set = new HashSet<>(pack.getSet());
+      this.list = new ArrayList<>(pack.getList());
     }
 
-    //int nThreads = Runtime.getRuntime().availableProcessors()
-    public List<Set<Integer>> maxPack(int nThreads, boolean showAllPackagesInConsole) {
-        try {
-            ExecutorService executor = Executors.newFixedThreadPool(nThreads);
-            for (var element : subsetList) {
-                executor.execute(() -> process(element, new Pack(), new CopyOnWriteArrayList<>(subsetList)));
-            }
-            executor.shutdown();
-            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            log.error("Error occurred while waiting for executor to terminate.", e);
-        }
+    public boolean add(Set<Integer> set) {
+      if (this.set.addAll(set)) {
+        this.list.add(set);
+        return true;
+      }
+      return false;
+    }
+  }
 
-        if (showAllPackagesInConsole) {
-            packSet.forEach(x -> log.info(x.toString()));
-        }
-        return packSet.stream()
-                .max(Comparator.comparingInt(Set::size))
-                .map(ArrayList::new)
-                .orElseGet(ArrayList::new);
+  //int nThreads = Runtime.getRuntime().availableProcessors()
+  public List<Set<Integer>> maxPack(int nThreads, boolean showAllPackagesInConsole) {
+    try {
+      ExecutorService executor = Executors.newFixedThreadPool(nThreads);
+      List<Set<Integer>> sets = new CopyOnWriteArrayList<>(subsetList);
+
+      for (var element : subsetList) {
+        List<Set<Integer>> currentSets = new CopyOnWriteArrayList<>(sets);
+        executor.execute(
+            () -> process(element, new Pack(), new CopyOnWriteArrayList<>(currentSets)));
+        sets.remove(element);
+      }
+
+      executor.shutdown();
+      executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      log.error("Error occurred while waiting for executor to terminate.", e);
     }
 
-    private void process(Set<Integer> subset, Pack pack, List<Set<Integer>> availableSubsets){
-        if (pack.add(subset) && mainSet.equals(pack.getSet())) {
-            packSet.add(new HashSet<>(pack.getList()));
-            return;
-        }
-
-        availableSubsets.remove(subset);
-        if (availableSubsets.isEmpty()) {
-            return;
-        }
-
-        for (var element : new ArrayList<>(availableSubsets)) {
-            process(element, new Pack(pack), new CopyOnWriteArrayList<>(availableSubsets));
-        }
+    if (showAllPackagesInConsole) {
+      packSet.forEach(x -> log.info(x.toString()));
     }
+    return packSet.stream()
+        .max(Comparator.comparingInt(Set::size))
+        .map(ArrayList::new)
+        .orElseGet(ArrayList::new);
+  }
+
+  private void process(Set<Integer> subset, Pack pack, List<Set<Integer>> availableSubsets) {
+    pack.add(subset);
+    availableSubsets.remove(subset);
+    if (availableSubsets.isEmpty()) {
+      if (mainSet.equals(pack.getSet())) {
+        packSet.add(new HashSet<>(pack.getList()));
+      }
+      return;
+    }
+
+    List<Set<Integer>> sets = new ArrayList<>(availableSubsets);
+    for (var element : availableSubsets) {
+      sets.remove(element);
+      process(element, new Pack(pack), new CopyOnWriteArrayList<>(sets));
+    }
+  }
 }
